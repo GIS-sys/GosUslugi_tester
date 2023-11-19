f4652c1415c00158dd7fd88bf98192f39f5f7235
EgorovGA <egorov.ga@minc.tech>
2023-11-20 02:17:39 +0300

    add convert scene

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ AUTH_EMAIL = "esiatest002@yandex.ru"
 AUTH_PASS = "11111111"
 AUTH_ROLE = "Фамилия002 Имя002  Отчество002"
 SERVICE_CODE = 618022
-SCENE_FILEPATH = "scenes/618022_reg_att.scn"
+#SCENE_FILEPATH = "scenes/618022_reg_att.scn"
+SCENE_FILEPATH = "scenes/618022_convert.scn"
 #SERVICE_CODE = 619069
 #SCENE_FILEPATH = "scenes/619069.scn"
 
