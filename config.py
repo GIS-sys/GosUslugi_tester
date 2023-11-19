37253922e2f7ca7068cbda812daaddf675d2ca07
EgorovGA <egorov.ga@minc.tech>
2023-11-20 02:47:30 +0300

    add missed restore scene

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ AUTH_PASS = "11111111"
 AUTH_ROLE = "Фамилия002 Имя002  Отчество002"
 SERVICE_CODE = 618022
 #SCENE_FILEPATH = "scenes/618022_reg_att.scn"
-SCENE_FILEPATH = "scenes/618022_convert.scn"
+#SCENE_FILEPATH = "scenes/618022_convert.scn"
+SCENE_FILEPATH = "scenes/618022_missed_restore.scn"
 #SERVICE_CODE = 619069
 #SCENE_FILEPATH = "scenes/619069.scn"
 
