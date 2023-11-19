282f38ac0dddd064650a86cdefabb2d301800fd6
EgorovGA <egorov.ga@minc.tech>
2023-11-19 03:28:31 +0300

    add list selection

--- tester.py
+++ tester.py
@@ -19,11 +19,14 @@ class Tester:
     def __del__(self):
         self.driver.quit()
 
+    def _waitGetElement(self, args):
+        return WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(args))
+
     def auth(self, email, password):
         self.driver.get("https://esia-portal1.test.gosuslugi.ru/login/")
-        inputEmail = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.ID, "login")))
-        inputPassword = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.ID, "password")))
-        buttonAuth = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//button[normalize-space()="Войти"]')))
+        inputEmail = self._waitGetElement((By.ID, "login"))
+        inputPassword = self._waitGetElement((By.ID, "password"))
+        buttonAuth = self._waitGetElement((By.XPATH, '//button[normalize-space()="Войти"]'))
         if len(inputEmail) != 1 or len(inputPassword) != 1 or len(buttonAuth) != 1:
             raise Exception("Authentification page is not loaded properly")
         inputEmail[0].send_keys(email)
@@ -32,7 +35,7 @@ class Tester:
 
     def role(self, role):
         self.driver.get("https://svcdev-roles.test.gosuslugi.ru/")
-        buttonRole = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'role')))
+        buttonRole = self._waitGetElement((By.CLASS_NAME, 'role'))
         for button in buttonRole:
             if button.get_property("title").startswith(role):
                 tryN(button.click, 5, 1)
@@ -42,7 +45,7 @@ class Tester:
     def chooseService(self, code):
         self.driver.get(f"http://svcdev-beta.test.gosuslugi.ru/{code}/1/form")
         try:
-            buttonAuth = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//span[normalize-space()="Начать заново"]')))
+            buttonAuth = self._waitGetElement((By.XPATH, '//span[normalize-space()="Начать заново"]'))
         except selenium.common.exceptions.TimeoutException:
             print("Отсутствует кнопка 'Начать заново', полагаю, что услуга уже началась")
             return
@@ -51,13 +54,25 @@ class Tester:
         tryN(buttonAuth[0].click, 5, 1)
 
     def initiate(self):
-        for action in [["click", "Начать"], ["click", "Заявитель"]]:
-            if action[0] == "click":
-                button = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f"//button[contains(text(),'{action[1]}')]")))
-                print(button)
+        for action in [["button", "Начать"], ["button", "Заявитель"], ["list", "Перечень", "Исключить патентного"]]:
+            if action[0] == "button":
+                button = self._waitGetElement((By.XPATH, f"//epgu-constructor-screen-resolver//button//*[contains(text(),'{action[1]}')]"))
                 button[0].click()
+            if action[0] == "list":
+                listOpen = self._waitGetElement((By.XPATH, f"//epgu-constructor-screen-resolver//epgu-constructor-dropdown[contains(.,'{action[1]}')]//input[contains(@class,'focusable-input')]"))
+                listOpen[0].click()
+                listEl = self._waitGetElement((By.XPATH, f"//epgu-constructor-screen-resolver//epgu-constructor-dropdown[contains(.,'{action[1]}')]//*[contains(@class,'dropdown-list-wrapper')]//*[contains(@class,'dropdown-item')]//*[contains(.,'{action[2]}')]"))
+                listEl[0].click()
+
+    def tmp(self):
+        self.driver.get(f"http://localhost:8080")
+        allElements = self._waitGetElement((By.XPATH, "//div//span[contains(@class, 'a')][contains(@class, 'b')]"))
+        for x in allElements:
+            print(x.tag_name, x.text, x.get_attribute("innerHTML"))
+        exit()
 
     def run(self):
+        #self.tmp()
         self.auth("esiatest002@yandex.ru", "11111111")
         self.role("Фамилия002 Имя002  Отчество002")
         self.chooseService(619069)
