94feb006e4006f003b35f1cccdebff2227910b40
EgorovGA <egorov.ga@minc.tech>
2023-11-30 12:38:39 +0300

    increase basic delay

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ AUTH_ROLE = "Сергеев Сергей  Сергеевич"
 
 # delay between each interaction with browser
 # if you have problems with script missing some clicks, consider setting it to a higher value
-DELAY_BETWEEN_ACTIONS = 0.1
+DELAY_BETWEEN_ACTIONS = 0.15
 # if set to True, will close browser automatically after completing all actions
 CLOSE_AFTER_TEST = True
 # if previous setting is set to True, this will determine how many seconds will browser wait before automatically closing
